6ded9938e36ebcb535c8d3f492d89874aa950159
Benjamin F. Drews <bfd2@tcl216-2.cs.williams.edu>
2016-01-05 13:27:47 -0500

    3 Distinct code areas

--- assets/game.js
+++ assets/game.js
@@ -10,30 +10,56 @@ window.onload = function() {
         Game.init();
 
         // Add the containers to our HTML page
+        document.getElementById('wsrl-avatar-display').appendChild(   Game.getDisplay('avatar').getContainer());
         document.getElementById('wsrl-main-display').appendChild(   Game.getDisplay('main').getContainer());
+        document.getElementById('wsrl-message-display').appendChild(   Game.getDisplay('message').getContainer());
     }
 };
 
 var Game = {
   DISPLAYS: {
+    avatar: {
+      w: 20,
+      h: 24,
+      o: null
+    },
     main: {
       w: 80,
       h: 24,
+      o: null
+    },
+    message: {
+      w: 100,
+      h: 6,
       o: null
     }
   },
   init: function () {
     console.log("RogueLike initialization");
-    this.DISPLAYS.main.o = new ROT.Display({width:Game.DISPLAYS.main.w, height:Game.DISPLAYS.main.h});
-    this.renderMain();
+    for (var displayName in this.DISPLAYS) {
+      if(this.DISPLAYS.hasOwnProperty(displayName)){
+        this.DISPLAYS[displayName].o = new ROT.Display({width:Game.DISPLAYS[displayName].w, height:Game.DISPLAYS[displayName].h});
+      }
+    }
+    this.renderAll();
   },
   getDisplay: function(displayName){
     return Game.DISPLAYS[displayName].o;
+  },
+  renderAll: function() {
+    this.renderAvatar();
+    this.renderMain();
+    this.renderMessage();
+  },
+  renderAvatar: function() {
+      this.DISPLAYS.avatar.o.drawText(2,3,"Avatar Display");
   },
   renderMain: function() {
     for (var i = 0; i < 5; i++) {
       this.DISPLAYS.main.o.drawText(2,3 +i,"Hello, world!");
     }
-
+  },
+  renderMessage: function() {
+      this.DISPLAYS.message.o.drawText(2,3,"Message Display");
   }
 };
